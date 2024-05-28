06e2156c52067c91f79a4dd3d15805fde168eeea
PritiTech <pritikl84@gmail.com>
2024-05-28 16:06:09 +0530

    update s3 bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform-bucket" {
-    bucket = "our-terraform-tfstate-file-bucket"
+    bucket = "our-terraform-tfstate-file-bucket23"
     tags = {
         Name = "first-bucket"
         Env = "Dev"
